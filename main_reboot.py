9cc6438f5e9a113cc42aa887d54d04c48e1dbc52
SaiDheerajPeketi <saidheerajpeketi@gmail.com>
2023-08-09 20:50:58 +0530

    Deployable Code

--- main_reboot.py
+++ main_reboot.py
@@ -24,6 +24,8 @@ try:
         prev_stocks.append(item[1])
     for item in prices:
         prev_prices.append(float(item[1]))
+    if len(prices) != len(stocks):
+        raise Exception("Stocks size is not equal to Price size")
     for i in range(0, len(prev_stocks)):
         prev_stocks[i] = "".join(char if char.isalpha() or char.isspace() else "" for char in prev_stocks[i])
 except:
@@ -44,45 +46,76 @@ while True:
             curr_stocks[i] = "".join(char if char.isalpha() or char.isspace() else "" for char in curr_stocks[i])
         for item in prices:
             curr_prices.append(float(item[1]))
-    except:
-        print("Failed to fill current stocks")
+        if len(prices) != len(stocks):
+            raise Exception("Stocks size is not equal to Price size")
+    except Exception as e:
+        print("Failed to fill current stocks ", e)
+        continue
 
-    combined_stocks = set(curr_stocks + prev_stocks)
-    for i in range(0, len(curr_stocks)):
-        item = curr_stocks[i]
-        curr_holdings = [h[0] for h in holdings]
+    try:
+        try:
 
-        if item not in curr_holdings and item not in prev_stocks:
-            curr_price = curr_prices[i]
-            print(f"BUY {item} at {curr_price}")
-            holdings.append((item, curr_price, curr_price))
-            new_stocks.append(item)
+            combined_stocks = set(curr_stocks + prev_stocks)
+            for i in range(0, len(curr_stocks)):
+                try:
+                    item = curr_stocks[i]
+                    curr_holdings = [h[0] for h in holdings]
+                except Exception as e:
+                    print("Current Stocks Index ", e)
+                    continue
+                try:
+                    if item not in curr_holdings and item not in prev_stocks:
+                        try:
+                            curr_price = curr_prices[i]
+                        except Exception as e:
+                            print("Current Price Index ", e)
+                            continue
 
-        if item in curr_holdings:
-            #print("Done")
-            holding_index = curr_holdings.index(item)
-            prev_index = prev_stocks.index(item)
-            prev_price = prev_prices[prev_index]
-            curr_price = curr_prices[i]
-            print(item)
-            if (curr_price - prev_price <= lower_threshold) or (
-                    curr_price - prev_price >= upper_threshold) or item not in curr_stocks:
-                print(f"SELL {item} at {curr_price}")
-                holdings.pop(holding_index)
-                # del holdings[holding_index]
-            else:
-                # print(curr_stocks)
-                holdings[holding_index] = (holdings[holding_index][0], holdings[holding_index][1], curr_price)
-    modified = []
-    for i in range(0, len(holdings)):
-        stock = holdings[i][0]
-        if stock not in curr_stocks:
-            print(f"SELL {stock} at {lower_threshold}")
-            modified.append(stock)
-    for stock in modified:
-        curr_holdings = [item[0] for item in holdings]
-        index = curr_holdings.index(stock)
-        holdings.pop(index)
-    print(holdings)
-    prev_stocks = curr_stocks
-    prev_prices = curr_prices
+                        print(f"BUY {item} at {curr_price}")
+                        holdings.append((item, curr_price, curr_price))
+                        new_stocks.append(item)
+
+                    if item in curr_holdings:
+                        # print("Done")
+                        holding_index = curr_holdings.index(item)
+                        prev_index = prev_stocks.index(item)
+                        prev_price = prev_prices[prev_index]
+                        curr_price = curr_prices[i]
+                        print(item)
+                        if (curr_price - prev_price <= lower_threshold) or (
+                                curr_price - prev_price >= upper_threshold) or item not in curr_stocks:
+                            print(f"SELL {item} at {curr_price}")
+                            holdings.pop(holding_index)
+                            # del holdings[holding_index]
+                        else:
+                            # print(curr_stocks)
+                            holdings[holding_index] = (
+                            holdings[holding_index][0], holdings[holding_index][1], curr_price)
+                except Exception as e:
+                    print("Buy Sell Conditions", e)
+                    continue
+        except Exception as e:
+            print("")
+            continue
+        try:
+            modified = []
+            for i in range(0, len(holdings)):
+                stock = holdings[i][0]
+                if stock not in curr_stocks:
+                    print(f"SELL {stock} at {lower_threshold}")
+                    modified.append(stock)
+            for stock in modified:
+                curr_holdings = [item[0] for item in holdings]
+                index = curr_holdings.index(stock)
+                holdings.pop(index)
+            print(holdings)
+            prev_stocks = curr_stocks
+            prev_prices = curr_prices
+        except Exception as e:
+            print("Removing from Holdings ", e)
+            continue
+    except Exception as e:
+        print("Infinite While ", e)
+        continue
+    if len(modified) != 0 or len(new_stocks) != 0:
+        send_email(modified + new_stocks)
